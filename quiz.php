dedb8e738b0677af01812ebc196f67f6abdfd548
migue <migue@DESKTOP-231QL02>
2020-11-30 23:27:53 +0000

    Alteração

--- quiz.php
+++ quiz.php
@@ -148,7 +148,7 @@ if(isset($_POST['signaturesubmit'])){
    <div class="container">
     <div class="row justify-content-between">
       <div class="col-12 col-md d-flex align-items-center">
-       <p class="mb-0 phone"><span class="mailus">Phone no:</span> <a href="#">+00 1234 567</a> or <span class="mailus">email us:</span> <a href="#">emailsample@email.com</a></p>
+       <p class="mb-0 phone"><span class="mailus">Contacto:</span> <a href="#">+351 936669133</a> or <span class="mailus">email:</span> <a href="#">softdiet@geral.pt</a></p>
      </div>
      <div class="col-12 col-md d-flex justify-content-md-end">
        <div class="social-media">
@@ -167,6 +167,7 @@ if(isset($_POST['signaturesubmit'])){
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
  <div class="container">
    <img src="https://softdiet.pt/img/logo.jpg" width="130" height="80">
+   <a class="navbar-brand" href="index.html"><img src="https://scontent.fopo2-2.fna.fbcdn.net/v/t1.0-9/122958446_640492499969556_8572259873588969268_o.png?_nc_cat=105&ccb=2&_nc_sid=09cbfe&_nc_ohc=tRaSdZ7jf7QAX94tIvJ&_nc_oc=AQn8LNUqF0VckvHhOltMS2omxehE0e6tDrOlOAYvutmuXKuO16grgJLf5uKI1OpTC49SmgP0EnIovp7Vm0Pb6Ki8&_nc_ht=scontent.fopo2-2.fna&oh=0ee814649f6e9e188e1690556fd878a1&oe=5FE5FC3C" style="width:50px; height:50px">&nbsp;<p id="span-title-softdiet">Soft<span>diet</span></p></a>
    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
      <span class="oi oi-menu"></span> Menu
    </button>
@@ -174,12 +175,12 @@ if(isset($_POST['signaturesubmit'])){
    <div class="collapse navbar-collapse" id="ftco-nav">
      <ul class="navbar-nav ml-auto">
        <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
-       <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-       <li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
-       <li class="nav-item"><a href="cases.html" class="nav-link">Case Study</a></li>
+       <li class="nav-item"><a href="about.html" class="nav-link">Sobre</a></li>
+       <li class="nav-item"><a href="services.html" class="nav-link">Preços</a></li>
+       <li class="nav-item"><a href="cases.html" class="nav-link">Registar</a></li>
        <li class="nav-item active"><a href="blog.html" class="nav-link">Blog</a></li>
-       <li class="nav-item"><a href="contact.html" class="nav-link">Contact us</a></li>
-       <li class="nav-item cta"><a href="#" class="nav-link">Free Consultation</a></li>
+       <li class="nav-item"><a href="contact.html" class="nav-link">Contacte-nos</a></li>
+       <li class="nav-item cta"><a href="#" class="nav-link">Fale Connosco</a></li>
 
      </ul>
    </div>
@@ -307,8 +308,8 @@ if(isset($_POST['signaturesubmit'])){
     <div class="row mb-5">
       <div class="col-md">
         <div class="ftco-footer-widget mb-4">
-          <h2 class="ftco-footer-logo">IT<span>solution</span></h2>
-          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <h2 class="ftco-footer-logo">Siga-nos nas redes sociais</h2>
+          <p>Fique a par de todo o nosso trabalho entrando numa das redes sociais abaixo. Visite já a nossa página, e fique a par das ultimas novidades</p>
           <ul class="ftco-footer-social list-unstyled mt-2">
             <li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
             <li class="ftco-animate"><a href="#"><span class="fa fa-facebook"></span></a></li>
@@ -320,12 +321,12 @@ if(isset($_POST['signaturesubmit'])){
         <div class="ftco-footer-widget mb-4 ml-md-5">
           <h2 class="ftco-heading-2">Explore</h2>
           <ul class="list-unstyled">
-            <li><a href="#" class="py-2 d-block">About</a></li>
-            <li><a href="#" class="py-2 d-block">Contact</a></li>
-            <li><a href="#" class="py-2 d-block">What We Do</a></li>
-            <li><a href="#" class="py-2 d-block">Plans &amp; Pricing</a></li>
-            <li><a href="#" class="py-2 d-block">Refund Policy</a></li>
-            <li><a href="#" class="py-2 d-block">Call Us</a></li>
+            <li><a href="#" class="py-2 d-block">Sobre</a></li>
+            <li><a href="#" class="py-2 d-block">Registo</a></li>
+            <li><a href="#" class="py-2 d-block">Funcionalidades</a></li>
+            <li><a href="#" class="py-2 d-block">Planos &amp; Preços</a></li>
+            <li><a href="#" class="py-2 d-block">Politica de Reembolso</a></li>
+            <li><a href="#" class="py-2 d-block">Contacte-nos</a></li>
           </ul>
         </div>
       </div>
@@ -333,23 +334,23 @@ if(isset($_POST['signaturesubmit'])){
        <div class="ftco-footer-widget mb-4">
         <h2 class="ftco-heading-2">Legal</h2>
         <ul class="list-unstyled">
-          <li><a href="#" class="py-2 d-block">Join Us</a></li>
+          <li><a href="#" class="py-2 d-block">Junte-se a nós</a></li>
           <li><a href="#" class="py-2 d-block">Blog</a></li>
-          <li><a href="#" class="py-2 d-block">Privacy &amp; Policy</a></li>
-          <li><a href="#" class="py-2 d-block">Terms &amp; Conditions</a></li>
-          <li><a href="#" class="py-2 d-block">Careers</a></li>
-          <li><a href="#" class="py-2 d-block">Contact</a></li>
+          <li><a href="#" class="py-2 d-block">Politica &amp; Privacidade</a></li>
+          <li><a href="#" class="py-2 d-block">Termos &amp; Condições</a></li>
+          <li><a href="#" class="py-2 d-block">Planos Alimentares</a></li>
+          <li><a href="#" class="py-2 d-block">Contacto</a></li>
         </ul>
       </div>
     </div>
     <div class="col-md">
       <div class="ftco-footer-widget mb-4">
-       <h2 class="ftco-heading-2">Have a Questions?</h2>
+       <h2 class="ftco-heading-2">Endereço</h2>
        <div class="block-23 mb-3">
          <ul>
-           <li><span class="icon fa fa-map marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-           <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-           <li><a href="#"><span class="icon fa fa-paper-plane pr-4"></span><span class="text">info@yourdomain.com</span></a></li>
+           <li><span class="icon fa fa-map marker"></span><span class="text">Rua Elias Garcia, Nº 23 4490-650 Póvoa de Varzim Portugal</span></li>
+           <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">(+351) 936669133</span></a></li>
+           <li><a href="#"><span class="icon fa fa-paper-plane pr-4"></span><span class="text">softdiet@geral.pt</span></a></li>
          </ul>
        </div>
      </div>
@@ -359,7 +360,7 @@ if(isset($_POST['signaturesubmit'])){
   <div class="col-md-12 text-center">
 
     <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-      Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+      Copyright &copy;<script>document.write(new Date().getFullYear());</script> Softdiet
       <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
     </div>
   </div>
