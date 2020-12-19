68ffb3b5a27e3f446babbc37b2548ce72d94fd2d
migue <migue@DESKTOP-231QL02>
2020-12-19 01:08:49 +0000

    alterações

--- questionario.php
+++ questionario.php
@@ -15,7 +15,7 @@
         }
     </script>
     <head>
-        <title>SoftDiet - QuizWebApp</title>
+        <title>SoftDiet</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
